9fc5d6fe0351e469eca6840f6ca802b7bcccf598
ErJun Wu <defyto13579@gmail.com>
2024-07-02 17:38:41 +0800

    update

--- apps/forms.py
+++ apps/forms.py
@@ -4,15 +4,50 @@ from django.contrib.auth.models import User
 from .models import MusicEvent
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={
+            'class': 'border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={
+            'class': 'border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
 
 class EditForm(forms.ModelForm):
+    title = forms.CharField(
+        label="Title",
+        widget=forms.TextInput(attrs={
+            'class': 'w-full border rounded-md py-2 px-3 focus:outline-none focus:border-blue-500'
+        })
+    )
+
     class Meta:
         model = MusicEvent
         fields = ['title']
